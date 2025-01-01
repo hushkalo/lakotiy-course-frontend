95b4acf44ab5c1b7be165dc375d96d9e2bd673b1
Herman2201 <ushkalo.herman@gmail.com>
2025-01-01 23:09:56 +0200

    delete router and extra files

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 // vite.config.ts
 import path from "path";
-import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), viteReact()],
+  plugins: [viteReact()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
